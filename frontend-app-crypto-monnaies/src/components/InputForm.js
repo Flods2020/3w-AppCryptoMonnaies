ef5541aab6e144b81faa99c6c81e2c81d14cb191
Flods2020 <florian.desieter2020@campus-eni.fr>
2023-10-13 17:43:50 +0200

    1743 131023

--- frontend-app-crypto-monnaies/src/components/InputForm.js
+++ frontend-app-crypto-monnaies/src/components/InputForm.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { BsCheckCircle } from "react-icons/bs";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const InputForm = (props) => {
   return (
     <div>
-      <label>{props.label} ::: </label>
+      <label>
+        {props.label} :::
+        <span className={props.valid ? "valid" : "hide"}>
+          <BsCheckCircle />
+        </span>
+        <span className={props.valid || !props.varia ? "hide" : "invalid"}>
+          <AiOutlineCloseCircle />
+        </span>
+      </label>
       <input
         type={props.type}
+        id={props.name}
         onChange={props.handleChange}
-        name={props.id}
-        autoComplete="off"
         required
+        // name={props.id}
+        onFocus={props.onFocus}
+        onBlur={props.onBlur}
       />
     </div>
   );
--- frontend-app-crypto-monnaies/src/pages/Register.js
+++ frontend-app-crypto-monnaies/src/pages/Register.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import Form from "../components/Form";
 import InputForm from "../components/InputForm";
-import { FaCheck, FaTimes, FaInfoCircle } from "react-icons/fa";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const USER_REGEX = /^[a-zA-Z0-9_]{3,23}$/;
 const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,24}$/;
@@ -71,23 +70,63 @@ const Register = () => {
         <InputForm
           handleChange={(e) => setUser(e.target.value)}
           type={"text"}
-          id={"username"}
-          ref={userRef}
+          name={"username"}
+          innerRef={userRef}
           label={"Nom/Pseudo"}
+          valid={validName}
+          varia={user}
           aria-invalid={validName ? "false" : "true"}
           aria-describedby="uidnote"
           onFocus={() => setUserFocus(true)}
           onBlur={() => setUserFocus(false)}
         />
+        <p
+          id="uidnote"
+          className={
+            userFocus && user && !validName ? "instructions" : "offscreen"
+          }
+        >
+          <AiOutlineCloseCircle /> <br />
+          Le champs doit commencer par une lettre.
+          <br />
+          Le champs doit contenir de 3 à 24 caractères.
+          <br />
+          Sont permis lettres, chiffres et traits d'union _.
+          <br />
+        </p>
+
+        <InputForm
+          handleChange={(e) => setPwd(e.target.value)}
+          type={"password"}
+          name={"password"}
+          // innerRef={userRef}
+          label={"Mot de passe"}
+          valid={validPwd}
+          varia={pwd}
+          autoComplete="off"
+          aria-invalid={validPwd ? "false" : "true"}
+          aria-describedby="pwdnote"
+          onFocus={() => setPwdFocus(true)}
+          onBlur={() => setPwdFocus(false)}
+        />
+        <p
+          id="pwdnote"
+          className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
+        >
+          <AiOutlineCloseCircle /> <br />
+          Le champs doit contenir de 8 à 24 caractères.
+          <br />
+          Le champs doit contenir une majuscule, une minuscule, un chiffre et un
+          caractère spécial.
+          <br />
+          Sont permis lettres, chiffres et "!@#$%".
+          <br />
+        </p>
+
         <InputForm
           // handleChange={(e) => setEmail(e.target.value)}
           label={"Email"}
           type={"email"}
-        />
-        <InputForm
-          // handleChange={(e) => setPassword(e.target.value)}
-          label={"Mot de passe"}
-          type={"password"}
         />
       </Form>
     </>
