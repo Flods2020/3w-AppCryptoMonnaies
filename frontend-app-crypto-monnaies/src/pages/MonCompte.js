175f9a59ebdba9ae0bb25def29797cdf9f138fc9
Flods2020 <florian.desieter2020@campus-eni.fr>
2023-11-22 02:10:43 +0100

    0210 221123

--- frontend-app-crypto-monnaies/src/pages/MonCompte.js
+++ frontend-app-crypto-monnaies/src/pages/MonCompte.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/index.scss";
 import { useSelector } from "react-redux";
 import { TbPassword } from "react-icons/tb";
@@ -6,33 +6,109 @@ import { TbPassword } from "react-icons/tb";
 const MonCompte = () => {
   const userProfileData = useSelector((state) => state.users);
 
+  const [newPseudo, setNewPseudo] = useState("");
+
+  const [displayPseudoInput, setDisplayPseudoInput] = useState(false);
+  const [displayPasswordInput, setDisplayPasswordInput] = useState(false);
+  const [displayEmailInput, setDisplayEmailInput] = useState(false);
+
+  const pseudoInput = document.getElementById("pseudo-input");
+
+  const saveInputValue = (constName, setFunc, displ) => {
+    // appeler axios patch
+    console.log(constName);
+    setFunc(!displ);
+  };
+
+  useEffect(() => {
+    // if (newPseudo !== "") {
+    //   newPseudo;
+    // } else {
+    //   setNewPseudo(userProfileData.pseudo);
+    // }
+    console.log("newPseudo ::: ", newPseudo);
+  }, [newPseudo]);
+
   return (
     <div className="acm-home-container">
       <div className="acm-monCompte-container">
         <h2>Mon Compte</h2>
         <div className="id-container">
           <h3>Modifier mes identifiants</h3>
-          <div className="modif-id">
-            <span>{userProfileData.pseudo}</span>
-            <div className="id-modif-btn" id="pseudo-btn">
-              Modifier Pseudo
+          <div className="modif-id-container">
+            <div className="modif-id">
+              {!displayPseudoInput ? (
+                // <span>{userProfileData.pseudo}</span>
+                <span>{newPseudo ? newPseudo : userProfileData.pseudo}</span>
+              ) : (
+                <input
+                  className="id-modif-inputs"
+                  id="pseudo-input"
+                  type="text"
+                  autoComplete="off"
+                  autoFocus={true}
+                  onChange={(e) => setNewPseudo(e.target.value)}
+                />
+              )}
+              <div
+                className="id-modif-btn"
+                id="pseudo-btn"
+                onClick={() =>
+                  !displayPseudoInput
+                    ? setDisplayPseudoInput(!displayPseudoInput)
+                    : (saveInputValue(
+                        newPseudo,
+                        setDisplayPseudoInput,
+                        displayPseudoInput
+                      ),
+                      setNewPseudo(pseudoInput.value))
+                }
+              >
+                Modifier Pseudo
+              </div>
             </div>
-          </div>
-          <div className="modif-id">
-            <span>
-              <TbPassword />
-              <TbPassword />
-              <TbPassword />
-              <TbPassword />
-            </span>
-            <div className="id-modif-btn" id="password-btn">
-              Modifier Mot de passe
+            <div className="modif-id">
+              {!displayPasswordInput ? (
+                <span>
+                  <TbPassword />
+                  <TbPassword />
+                  <TbPassword />
+                  <TbPassword />
+                </span>
+              ) : (
+                <input
+                  className="id-modif-inputs"
+                  id="password-input"
+                  type="text"
+                  onChange={(e) => console.log(e.target.value)}
+                />
+              )}
+              <div
+                className="id-modif-btn"
+                id="password-btn"
+                onClick={() => setDisplayPasswordInput(!displayPasswordInput)}
+              >
+                Modifier Mot de passe
+              </div>
             </div>
-          </div>
-          <div className="modif-id">
-            <span>{userProfileData.email.replace("@", " @")}</span>
-            <div className="id-modif-btn" id="email-btn">
-              Modifier Email
+            <div className="modif-id">
+              {!displayEmailInput ? (
+                <span>{userProfileData.email}</span>
+              ) : (
+                <input
+                  className="id-modif-inputs"
+                  id="email-input"
+                  type="text"
+                  onChange={(e) => console.log(e.target.value)}
+                />
+              )}
+              <div
+                className="id-modif-btn"
+                id="email-btn"
+                onClick={() => setDisplayEmailInput(!displayEmailInput)}
+              >
+                Modifier Email
+              </div>
             </div>
           </div>
         </div>
